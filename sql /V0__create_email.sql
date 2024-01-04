1c4137df5de810590e7c57472daced73c1c21d02
Iliyan Yordanov <76901712+Iliyan31@users.noreply.github.com>
2024-01-04 13:39:23 +0200

    Update V0__create_email.sql

--- sql /V0__create_email.sql
+++ sql /V0__create_email.sql
@@ -1,6 +1,6 @@
 create table email (
     email_id serial primary key,
-    email_address varchar(100),
-    email_type char(1) not null,
+    email_address varchar2(100),
+    email_type varchar2(1) not null,
     person_id int not null
 );
